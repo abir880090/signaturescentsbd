93f5fb9c8eab6a73eb3a93543bd8ffaad78a0162
abir880090 <97960842+abir880090@users.noreply.github.com>
2026-03-29 14:04:50 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,18 @@
-// ১. Firebase SDK (সব পেজের মাথায় দিতে হবে)
+// firebase-config.js e erokom thakte hobe:
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getDatabase, ref, set, push, onValue, update } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
-// ২. আপনার কনফিগারেশন (Firebase Console থেকে পাবেন)
 const firebaseConfig = {
-    apiKey: "AIzaSyAf1OrMuwbGJRDByY8LFi1fwlXZsfawwRI",
-    authDomain: "https://signature-scents-bd-fa4f9-default-rtdb.firebaseio.com",
-    databaseURL: "https://your-project-default-rtdb.firebaseio.com",
-    projectId: "signature-scents-bd-fa4f9",
-    storageBucket: "signature-scents-bd-fa4f9.firebasestorage.app",
-    messagingSenderId: "690465209021",
-    appId: "1:690465209021:web:a5bbde32d6360aeed515d4"
+  apiKey: "AIzaSyAf1OrMuwbGJRDbY8LFi1fwlXZsfawwRI",
+  authDomain: "signature-scents-bd-fa4f9.firebaseapp.com",
+  databaseURL: "https://signature-scents-bd-fa4f9-default-rtdb.firebaseio.com",
+  projectId: "signature-scents-bd-fa4f9",
+  storageBucket: "signature-scents-bd-fa4f9.firebasestorage.app",
+  messagingSenderId: "690465209021",
+  appId: "1:690465209021:web:a5bbde32d6360aeed515d4"
 };
-   
-// ৩. ইনিশিয়ালাইজ
+
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
+
+export { db, ref, push };
